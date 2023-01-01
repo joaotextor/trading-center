a99e4b2c29bbafc36e955dd4986e0e92a8cf1f0e
Joao Textor <joaotextor@gmail.com>
2023-01-01 14:18:14 -0300

    changed page title and removed comment

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,10 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>Trading Center</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
