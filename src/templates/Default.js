eec117943ddab5b40b4184c3e6fd94b0bf38691c
Joao Textor <joaotextor@gmail.com>
2023-01-01 14:19:56 -0300

    create Default template

---
+++ src/templates/Default.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+// import Footer from '../components/Footer'
+
+export default function Default({ children }) {
+    return (
+        <>
+            <Header>HEADER</Header>
+            { children }
+            <footer>FOOTER</footer>
+        </>
+    )
+}
