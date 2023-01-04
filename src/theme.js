59583a4c27418ac98061f22ef2f657c55fe64f96
Joao Textor <joaotextor@gmail.com>
2023-01-04 13:40:57 -0300

    add background and secondary color to theme

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,14 @@ const theme = createTheme({
     palette: {
         primary: {
             main: '#000000'
+        },
+        secondary: {
+            main: '#FFFFFF'
+        },
+        background: {
+            default: 'rgb(242, 244, 245)',
+            white: '#ffffff',
+            variantion: 'red',
         }
     }
 })
