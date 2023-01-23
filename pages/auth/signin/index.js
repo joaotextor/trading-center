70a1902be6590e85d63bf0ba60a954d3b2aa7fea
Joao Textor <joaotextor@gmail.com>
2023-01-22 21:19:50 -0300

    hide login form when already signed in.

--- pages/auth/signin/index.js
+++ pages/auth/signin/index.js
@@ -28,15 +28,7 @@ export default function Login({ APP_URL }) {
     const router = useRouter()
     const { data: session, status } = useSession()
     
-    /*
-    TODO | Place this below. Transform in normal IF
-    TODO | On 'else', returns the entire component
-     */
-    status === 'authenticated'
-    ? router.push(`${APP_URL}/user/dashboard`)
-    : null
-
-    const handleFormSubmit = async values => {
+        const handleFormSubmit = async values => {
     const signInStatus = await signIn('credentials', {
         email: values.email,
         password: values.password,
@@ -50,119 +42,124 @@ export default function Login({ APP_URL }) {
     router.push(`${APP_URL}/user/dashboard`)
  }
 
-const handleGoogleLogin = async () => {
-    await signIn('google', {
-        redirect: true,
-        callbackUrl: `${APP_URL}/user/dashboard`
-    })
-}
+    const handleGoogleLogin = async () => {
+        await signIn('google', {
+            redirect: true,
+            callbackUrl: `${APP_URL}/user/dashboard`
+        })
+    }
 
-  return (
-      <TemplateDefault>
-          <Container maxWidth="lg" component="main" sx={classes.container}>
-              <Typography component="h1" variant="h2" align="center" color="primary">
-                  Login to your account
-              </Typography>
-          </Container>
+    if (status === 'authenticated') {
+        router.push(`${APP_URL}/user/dashboard`)
+    } else {
 
-          <Container maxWidth="xs">  
-              <Box sx={classes.box}>
+        return (
+            <TemplateDefault>
+                <Container maxWidth="lg" component="main" sx={classes.container}>
+                    <Typography component="h1" variant="h2" align="center" color="primary">
+                        Login to your account
+                    </Typography>
+                </Container>
 
-                <Box sx={classes.googleLoginButton}>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        startIcon={
-                            <Image
-                                src="/images/logo_google.svg"
-                                alt="google"
-                                width={20}
-                                height={20}
-                            />
-                        }
-                        onClick={handleGoogleLogin}
-                    >Login with Google</Button>
-                </Box>
-                <Box sx={classes.orSeparator}>
-                    <span>OR</span>
-                </Box>
-                  <Formik
-                      initialValues={initialValues}
-                      validationSchema={validationSchema}
-                      onSubmit={handleFormSubmit}
-                  >
-                      {
-                          ({
-                              touched,
-                              values,
-                              errors,
-                              handleChange,
-                              handleSubmit,
-                              isSubmitting
-                          }) => {
-                              return (
-                                  <form onSubmit={handleSubmit}>
-                                      {
-                                        router.query.i == 1
-                                        ? <Alert severity='error' sx={classes.errorMessage}>Incorrect e-mail or password!</Alert>
-                                        : null
-                                      }  
-                                      <FormControl fullWidth error={errors.email && touched.email} sx={classes.formControl}>
-                                          <InputLabel sx={classes.inputLabel}>E-mail</InputLabel>
-                                          <Input
-                                              name="email"
-                                              value={values.email}
-                                              onChange={handleChange}
-                                          />
-                                          <FormHelperText>
-                                              {errors.email && touched.email ? errors.email : null}
-                                          </FormHelperText>
-                                      </FormControl>
+                <Container maxWidth="xs">  
+                    <Box sx={classes.box}>
 
-                                      <FormControl fullWidth error={errors.password && touched.password} sx={classes.formControl}>
-                                          <InputLabel sx={classes.inputLabel}>Password</InputLabel>
-                                          <Input
-                                              type="password"
-                                              name="password"
-                                              value={values.password}
-                                              onChange={handleChange}
-                                          />
-                                          <FormHelperText>
-                                              {errors.password && touched.password ? errors.password : null}
-                                          </FormHelperText>
-                                      </FormControl>
+                        <Box sx={classes.googleLoginButton}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                fullWidth
+                                startIcon={
+                                    <Image
+                                        src="/images/logo_google.svg"
+                                        alt="google"
+                                        width={20}
+                                        height={20}
+                                    />
+                                }
+                                onClick={handleGoogleLogin}
+                            >Login with Google</Button>
+                        </Box>
+                        <Box sx={classes.orSeparator}>
+                            <span>OR</span>
+                        </Box>
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={validationSchema}
+                            onSubmit={handleFormSubmit}
+                        >
+                            {
+                                ({
+                                    touched,
+                                    values,
+                                    errors,
+                                    handleChange,
+                                    handleSubmit,
+                                    isSubmitting
+                                }) => {
+                                    return (
+                                        <form onSubmit={handleSubmit}>
+                                            {
+                                                router.query.i == 1
+                                                ? <Alert severity='error' sx={classes.errorMessage}>Incorrect e-mail or password!</Alert>
+                                                : null
+                                            }  
+                                            <FormControl fullWidth error={errors.email && touched.email} sx={classes.formControl}>
+                                                <InputLabel sx={classes.inputLabel}>E-mail</InputLabel>
+                                                <Input
+                                                    name="email"
+                                                    value={values.email}
+                                                    onChange={handleChange}
+                                                />
+                                                <FormHelperText>
+                                                    {errors.email && touched.email ? errors.email : null}
+                                                </FormHelperText>
+                                            </FormControl>
 
-                                      {
-                                          isSubmitting
-                                          ? (
-                                              <CircularProgress sx={classes.loading}/>
-                                          )
-                                          : (
-                                              <Button
-                                                  type="submit"
-                                                  fullWidth
-                                                  variant="contained"
-                                                  color="primary"
-                                                  disabled={isSubmitting}
-                                                  sx={classes.submit}
-                                              >
-                                              Signin
-                                              </Button> 
-                                          )
-                                      }
+                                            <FormControl fullWidth error={errors.password && touched.password} sx={classes.formControl}>
+                                                <InputLabel sx={classes.inputLabel}>Password</InputLabel>
+                                                <Input
+                                                    type="password"
+                                                    name="password"
+                                                    value={values.password}
+                                                    onChange={handleChange}
+                                                />
+                                                <FormHelperText>
+                                                    {errors.password && touched.password ? errors.password : null}
+                                                </FormHelperText>
+                                            </FormControl>
 
-                                      
-                                  </form>
-                              )
-                          }
-                      }
+                                            {
+                                                isSubmitting
+                                                ? (
+                                                    <CircularProgress sx={classes.loading}/>
+                                                )
+                                                : (
+                                                    <Button
+                                                        type="submit"
+                                                        fullWidth
+                                                        variant="contained"
+                                                        color="primary"
+                                                        disabled={isSubmitting}
+                                                        sx={classes.submit}
+                                                    >
+                                                    Signin
+                                                    </Button> 
+                                                )
+                                            }
 
-                  </Formik>
-              </Box>
-          </Container>
-      </TemplateDefault>
-  )
+                                            
+                                        </form>
+                                    )
+                                }
+                            }
+
+                        </Formik>
+                    </Box>
+                </Container>
+            </TemplateDefault>
+        )
+    }
 }
 
 export async function getServerSideProps(context) {
