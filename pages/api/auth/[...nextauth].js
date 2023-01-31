41bbb9164347e60d837fbf6683c35ef846559646
Joao Textor <joaotextor@gmail.com>
2023-01-31 17:23:51 -0300

    apply a redirect callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -61,6 +61,14 @@ export const authOptions = {
 
       session.userId = token.uid
       return session
+    },
+
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
     }
   },
 
