33a300f8443e081eaabf4f4be3d88847a352a404
Joao Textor <joaotextor@gmail.com>
2023-01-31 15:46:17 -0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    APP_URL: process.env.APP_URL
   }
 }
 
