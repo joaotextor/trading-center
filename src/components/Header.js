42e09feedf74909b13be5e66b40897ca1d11a5f0
Joao Textor <joaotextor@gmail.com>
2023-01-31 16:22:30 -0300

    update signout callbackUrl

--- src/components/Header.js
+++ src/components/Header.js
@@ -85,7 +85,7 @@ export default function ButtonAppBar() {
               </MyLink>
               
               <Divider />
-              <MenuItem onClick={() => signOut({callbackUrl: '/'})}>Logout</MenuItem>
+              <MenuItem onClick={() => signOut({callbackUrl: `${process.env.APP_URL}`})}>Logout</MenuItem>
             </Menu>
           </Toolbar>
           </Container>
