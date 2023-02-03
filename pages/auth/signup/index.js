ca2187e6dc2bb2c3493fc35bad89b87ad7a87113
Joao Textor <joaotextor@gmail.com>
2023-02-03 13:54:55 -0300

    remove awsconfig

--- pages/auth/signup/index.js
+++ pages/auth/signup/index.js
@@ -1,5 +1,4 @@
-import Amplify, { Auth } from 'aws-amplify'
-import awsconfig from '../../../src/aws-exports'
+import { Auth } from 'aws-amplify'
 
 import {
     Box,
@@ -26,9 +25,6 @@ import useToasty from '../../../src/contexts/Toasty'
 import { classes } from '../../../styles/signup/styles'
 
 export default function Signup() {
-
-    Amplify.configure(awsconfig)
-    Auth.configure(awsconfig)
 
     const { setToasty } = useToasty()
     const router = useRouter()
